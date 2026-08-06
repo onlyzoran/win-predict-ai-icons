d96e27a2d17512766e5d2b67b363929dd48d5203
onlyzoran <42581800+onlyzoran@users.noreply.github.com>
2026-08-06 15:01:31 +0200

    Add boxing sport icon from Hugeicons. (#3)

---
+++ src/icons/IconBoxingGlove.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+withDefaults(
+  defineProps<{
+    size?: number | string
+    stroke?: number | string
+  }>(),
+  { size: 24, stroke: 2 },
+)
+</script>
+
+<template>
+  <!-- Source: Hugeicons boxing-glove-01 (MIT) -->
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="size"
+    :height="size"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    :stroke-width="stroke"
+    stroke-linecap="round"
+    stroke-linejoin="round"
+    class="tabler-icon tabler-icon-boxing-glove"
+  >
+    <path
+      d="M5.981 11.245c-.087 2.723 3.122 6.079 6.13 7.133m-6.13 -7.133c-.304 .523 -3.415 3.017 -3.47 4.073c-.263 2.098 3.301 6.039 6.05 6.173c1.025 .146 2.449 -1.58 3.892 -2.864c.795 -.706 1.94 -.87 6.524 -2.008c1.664 -.289 3.662 -3.158 .7 -5.59m-13.696 .216c.929 -2.2 2.533 -6.456 5.68 -8.137c1.278 -.726 4.598 -1.394 7.85 1.698c1.12 .941 3.06 3.002 1.254 5.378c-1.204 1.402 -3.024 .996 -3.2 3.422"
+    />
+  </svg>
+</template>
